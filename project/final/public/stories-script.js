1a06b988a6b011b68f67a7baeab375998332798c
Joshua Cook <jc157@email.sc.edu>
2023-12-08 12:51:12 -0500

    editing

--- project/final/public/stories-script.js
+++ project/final/public/stories-script.js
@@ -1,59 +1,61 @@
 const hideBar = () => {
-    document.getElementById("main-nav").classList.toggle("hidden");
-}
+  document.getElementById("main-nav").classList.toggle("hidden");
+};
 
 const getStories = async () => {
-    try {
-        const response = await fetch("https://joshc04.github.io/project/part5/stories.json");
-        const stories = await response.json();
-        return stories;
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const response = await fetch(
+      "https://joshc04.github.io/project/final/stories.json"
+    );
+    const stories = await response.json();
+    return stories;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const displayStories = async () => {
-    const stories = await getStories();
-    const storiesDisplay = document.getElementById("stories-main");
+  const stories = await getStories();
+  const storiesDisplay = document.getElementById("stories-main");
 
-    stories.forEach((story) => {
-        console.log(story);
-        storiesDisplay.appendChild(getStoriesContent(story));
-    });
+  stories.forEach((story) => {
+    console.log(story);
+    storiesDisplay.appendChild(getStoriesContent(story));
+  });
 };
 
 const getStoriesContent = (story) => {
-    const section = document.createElement("section");
-    section.classList.add("season");
+  const section = document.createElement("section");
+  section.classList.add("season");
 
-    const div = document.createElement("div");
-    div.classList.add("inner-div");
-    section.appendChild(div);
+  const div = document.createElement("div");
+  div.classList.add("inner-div");
+  section.appendChild(div);
 
-    const a = document.createElement("a");
-    a.classList.add("stories-a");
-    a.href = story.link;
-    div.appendChild(a);
+  const a = document.createElement("a");
+  a.classList.add("stories-a");
+  a.href = story.link;
+  div.appendChild(a);
 
-    const h1 = document.createElement("h1");
-    h1.classList.add("typography");
-    h1.innerHTML = story.title;
-    a.appendChild(h1);
+  const h1 = document.createElement("h1");
+  h1.classList.add("typography");
+  h1.innerHTML = story.title;
+  a.appendChild(h1);
 
-    const description = document.createElement("p");
-    description.classList.add("typography");
-    description.innerHTML = story.description;
-    div.appendChild(description);
+  const description = document.createElement("p");
+  description.classList.add("typography");
+  description.innerHTML = story.description;
+  div.appendChild(description);
 
-    const img = document.createElement("img");
-    img.classList.add("stories-img");
-    img.src = story.image;
-    section.appendChild(img);
+  const img = document.createElement("img");
+  img.classList.add("stories-img");
+  img.src = story.image;
+  section.appendChild(img);
 
-    return section;
+  return section;
 };
 
 window.onload = () => {
-    document.getElementById("home-img").onclick = hideBar;
-    displayStories();
+  document.getElementById("home-img").onclick = hideBar;
+  displayStories();
 };
